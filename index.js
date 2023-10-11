3bef8c0b3989b57a1f03834ca8d572f1575d73e4
Associate <Associate@roifmrwindt>
2023-10-11 11:55:52 +0530

    added teams route

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const foodRouter = require('./src/routes/foodRoute');
+const teamsRouter = require('./src/routes/teamsRoute')
 const connection = require("./src/configs/db");
 
 const app = express();
@@ -12,9 +13,9 @@ connection.getConnection();
 app.use(bodyParser.json());
 app.use(cors());
 app.use(foodRouter);
-
+app.use(teamsRouter);
 app.get('/', foodRouter);
-
+app.get('/',teamsRouter);
 app.listen(port, '0.0.0.0', () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
