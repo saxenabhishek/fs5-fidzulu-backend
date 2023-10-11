06c87661fb0748aed7367d5e532f59a1ad9764b5
Associate <Associate@roifmrwindt>
2023-10-11 14:19:32 +0530

    Team routes, and india tax change

--- index.js
+++ index.js
@@ -4,17 +4,21 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const toysRouter = require('./src/routes/toysRoute');
+const teamsRouter=require('./src/routes/teamsRoute')
 const connection = require("./src/configs/db");
 
 const app = express();
-const port = process.env.PORT || 3036;
+const port = process.env.PORT || 3033;
 connection.getConnection();
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(toysRouter);
+app.use(teamsRouter);
 
 app.get('/', toysRouter);
+app.get('/',teamsRouter);
+
 
 app.listen(port, '0.0.0.0', () => {
   console.log(`Example app listening at http://localhost:${port}`);
--- src/controllers/toysController.js
+++ src/controllers/toysController.js
@@ -6,7 +6,7 @@ const usToys = (result) => {
     for (let res of result) {
         const precision = 2;
         let price = res[5]
-        res[5] = Number((price + price * 0.18).toFixed(2));
+        res[5] = Number((price + price * 0.08).toFixed(2));
         
     }
     return result;
@@ -23,7 +23,7 @@ const ieToys = (result) => {
 const inToys = (result) => {
     for (let res of result) {
         let price = res[5]
-        res[5] = Number((price + price * 0.08).toFixed(2));
+        res[5] = Number((price + price * 0.18).toFixed(2));
     }
     return result;
 }
