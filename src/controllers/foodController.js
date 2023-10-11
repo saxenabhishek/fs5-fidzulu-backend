fe273265dc26b150a42b4c487b731489d9ec9594
Associate <Associate@roifmrwindt>
2023-10-11 10:16:24 +0530

    Chaned location to variable

--- src/controllers/foodController.js
+++ src/controllers/foodController.js
@@ -1,11 +1,42 @@
 const { getFoodDetails } = require('../models/productModel');
 const response = require('../configs/response');
+const usFood = (result) => {
+    for (let res of result) {
+        const precision = 2;
+        let price = res[5]
+        res[5] = Number((price + price * 0.18).toFixed(2));
+        
+    }
+    return result;
+}
+
+const irFood = (result) => {
+    for (let res of result) {
+        let price = res[5]
+        res[5] = Number((price + price * 0.23).toFixed(2));
+    }
+    return result;
+}
+
+const inFood = (result) => {
+    for (let res of result) {
+        let price = res[5]
+        res[5] = Number((price + price * 0.08).toFixed(2));
+    }
+    return result;
+}
 
  const getFood =  async (req, res) => {
         try {
-            const result = await getFoodDetails();
-            console.log(result);
-           
+            let result = await getFoodDetails();
+            switch(req.params.location){
+                case "US-NC" : result = usFood(result)
+                                break;
+                case "IE" : result = irFood(result)
+                                break;
+                case "IN" : result = inFood(result)
+                                break;
+            }
             if (result.length) {
                 return response(res, 'List of Food', 200, true, result);
             } else {
