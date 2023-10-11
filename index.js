196100f45b275236a3dc39c1a279bb585c9aeac1
Associate <Associate@roifmrwindt>
2023-10-11 13:32:34 +0530

    TeamsRoute fix

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const bikeRouter = require('./src/routes/bikeRoute');
+const teamsRouter = require('./src/routes/teamsRoute');
 const connection = require("./src/configs/db");
 
 const app = express();
@@ -12,9 +13,11 @@ connection.getConnection();
 app.use(bodyParser.json());
 app.use(cors());
 app.use(bikeRouter);
+app.use(teamsRouter);
 
 
 app.get('/', bikeRouter);
+app.get('/', teamsRouter);
 
 app.listen(port, '0.0.0.0', () => {
   console.log(`Example app listening at http://localhost:${port}`);
