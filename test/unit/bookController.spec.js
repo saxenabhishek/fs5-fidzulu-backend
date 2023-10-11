8e2f50347de21ee44f6e115174c6c2a2ba1abce8
Associate <Associate@roifmrwindt>
2023-10-11 16:51:33 +0530

    Updated BoookController tests

--- test/unit/bookController.spec.js
+++ test/unit/bookController.spec.js
@@ -1,23 +1,117 @@
 const { inBook } = require('../../src/controllers/bookController');
 const { irBook } = require('../../src/controllers/bookController');
 const { usBook } = require('../../src/controllers/bookController');
+const { getBook } = require('../../src/controllers/bookController');
+
+const {getBookDetails} = require('../../src/models/productModel');
+jest.mock('../../src/models/productModel');
 
 describe('usBook test', () => {
   it('should add 8% tax to each item in the result array', () => {
     const input =
       [
-        [201, "A Game of Thrones", "Fantasy", "George R.R. Martin", "Bantam", 11.79, 4.5, "https://shorturl.at/jyN45"],
-        [202, "To Kill a Mockingbird", "Fiction", "Harper Lee", "Harper Perennial", 15.33, 4.8, "https://shorturl.at/iovDL"],
-        [203, "The Da Vinci Code", "Mystery", "Dan Brown", "Vintage", 10.61, 4.2, "https://shorturl.at/blDHX"],
-        [204, "Harry Potter and the Order of the Phoenix", "Fantasy", "J.K. Rowling", "Scholastic", 17.69, 4.7, "https://shorturl.at/ivxSX"],
-        [205, "The Great Gatsby", "Classic", "F. Scott Fitzgerald", "Penguin", 9.43, 4, "https://shorturl.at/biQX4"]
+        {
+          "BookId": 201,
+          "BookName": "A Game of Thrones",
+          "Genre": "Fantasy",
+          "Author": "George R.R. Martin",
+          "Publisher": "Bantam",
+          "Price": 11.79,
+          "Ratings": 4.5,
+          "ImageUrl": "https://shorturl.at/jyN45"
+        },
+        {
+          "BookId": 202,
+          "BookName": "To kill a Mockingbird",
+          "Genre": "Fiction",
+          "Author": "Harper Lee",
+          "Publisher": "Harper Perennial",
+          "Price": 15.33,
+          "Ratings": 4.8,
+          "ImageUrl": "https://shorturl.at/iovDL"
+        },
+        {
+          "BookId": 203,
+          "BookName": "The Da Vinci Code",
+          "Genre": "Mystery",
+          "Author": "Dan Brown",
+          "Publisher": "Vintage",
+          "Price": 10.61,
+          "Ratings": 4.2,
+          "ImageUrl": "https://shorturl.at/blDHX"
+        },
+        {
+          "BookId": 204,
+          "BookName": "Harry Potter and the Order of the Phoenix",
+          "Genre": "Fantasy",
+          "Author": "J.K. Rowling",
+          "Publisher": "Scholastic",
+          "Price": 17.69,
+          "Ratings": 4.7,
+          "ImageUrl": "https://shorturl.at/ivxSX"
+        },
+        {
+          "BookId": 205,
+          "BookName": "The Great Gatsby",
+          "Genre": "Classic",
+          "Author": "F. Scott Fitzgerald",
+          "Publisher": "Penguin",
+          "Price": 9.43,
+          "Ratings": 4,
+          "ImageUrl": "https://shorturl.at/biQX4"
+        }
       ];
     const expectedOutput = [
-      [201, "A Game of Thrones", "Fantasy", "George R.R. Martin", "Bantam", 12.73, 4.5, "https://shorturl.at/jyN45"],
-      [202, "To Kill a Mockingbird", "Fiction", "Harper Lee", "Harper Perennial", 16.56, 4.8, "https://shorturl.at/iovDL"],
-      [203, "The Da Vinci Code", "Mystery", "Dan Brown", "Vintage", 11.46, 4.2, "https://shorturl.at/blDHX"],
-      [204, "Harry Potter and the Order of the Phoenix", "Fantasy", "J.K. Rowling", "Scholastic", 19.11, 4.7, "https://shorturl.at/ivxSX"],
-      [205, "The Great Gatsby", "Classic", "F. Scott Fitzgerald", "Penguin", 10.18, 4, "https://shorturl.at/biQX4"]
+      {
+        "BookId": 201,
+        "BookName": "A Game of Thrones",
+        "Genre": "Fantasy",
+        "Author": "George R.R. Martin",
+        "Publisher": "Bantam",
+        "Price": 12.73,
+        "Ratings": 4.5,
+        "ImageUrl": "https://shorturl.at/jyN45"
+      },
+      {
+        "BookId": 202,
+        "BookName": "To kill a Mockingbird",
+        "Genre": "Fiction",
+        "Author": "Harper Lee",
+        "Publisher": "Harper Perennial",
+        "Price": 16.56,
+        "Ratings": 4.8,
+        "ImageUrl": "https://shorturl.at/iovDL"
+      },
+      {
+        "BookId": 203,
+        "BookName": "The Da Vinci Code",
+        "Genre": "Mystery",
+        "Author": "Dan Brown",
+        "Publisher": "Vintage",
+        "Price": 11.46,
+        "Ratings": 4.2,
+        "ImageUrl": "https://shorturl.at/blDHX"
+      },
+      {
+        "BookId": 204,
+        "BookName": "Harry Potter and the Order of the Phoenix",
+        "Genre": "Fantasy",
+        "Author": "J.K. Rowling",
+        "Publisher": "Scholastic",
+        "Price": 19.11,
+        "Ratings": 4.7,
+        "ImageUrl": "https://shorturl.at/ivxSX"
+      },
+      {
+        "BookId": 205,
+        "BookName": "The Great Gatsby",
+        "Genre": "Classic",
+        "Author": "F. Scott Fitzgerald",
+        "Publisher": "Penguin",
+        "Price": 10.18,
+        "Ratings": 4,
+        "ImageUrl": "https://shorturl.at/biQX4"
+      }
     ];
     const result = usBook(input);
     expect(result).toEqual(expectedOutput);
@@ -34,18 +128,108 @@ describe('irBook test', () => {
   it('should add 23% tax to each item in the result array', () => {
     const input =
       [
-        [201, "A Game of Thrones", "Fantasy", "George R.R. Martin", "Bantam", 11.79, 4.5, "https://shorturl.at/jyN45"],
-        [202, "To Kill a Mockingbird", "Fiction", "Harper Lee", "Harper Perennial", 15.33, 4.8, "https://shorturl.at/iovDL"],
-        [203, "The Da Vinci Code", "Mystery", "Dan Brown", "Vintage", 10.61, 4.2, "https://shorturl.at/blDHX"],
-        [204, "Harry Potter and the Order of the Phoenix", "Fantasy", "J.K. Rowling", "Scholastic", 17.69, 4.7, "https://shorturl.at/ivxSX"],
-        [205, "The Great Gatsby", "Classic", "F. Scott Fitzgerald", "Penguin", 9.43, 4, "https://shorturl.at/biQX4"]
+        {
+          "BookId": 201,
+          "BookName": "A Game of Thrones",
+          "Genre": "Fantasy",
+          "Author": "George R.R. Martin",
+          "Publisher": "Bantam",
+          "Price": 11.79,
+          "Ratings": 4.5,
+          "ImageUrl": "https://shorturl.at/jyN45"
+        },
+        {
+          "BookId": 202,
+          "BookName": "To kill a Mockingbird",
+          "Genre": "Fiction",
+          "Author": "Harper Lee",
+          "Publisher": "Harper Perennial",
+          "Price": 15.33,
+          "Ratings": 4.8,
+          "ImageUrl": "https://shorturl.at/iovDL"
+        },
+        {
+          "BookId": 203,
+          "BookName": "The Da Vinci Code",
+          "Genre": "Mystery",
+          "Author": "Dan Brown",
+          "Publisher": "Vintage",
+          "Price": 10.61,
+          "Ratings": 4.2,
+          "ImageUrl": "https://shorturl.at/blDHX"
+        },
+        {
+          "BookId": 204,
+          "BookName": "Harry Potter and the Order of the Phoenix",
+          "Genre": "Fantasy",
+          "Author": "J.K. Rowling",
+          "Publisher": "Scholastic",
+          "Price": 17.69,
+          "Ratings": 4.7,
+          "ImageUrl": "https://shorturl.at/ivxSX"
+        },
+        {
+          "BookId": 205,
+          "BookName": "The Great Gatsby",
+          "Genre": "Classic",
+          "Author": "F. Scott Fitzgerald",
+          "Publisher": "Penguin",
+          "Price": 9.43,
+          "Ratings": 4,
+          "ImageUrl": "https://shorturl.at/biQX4"
+        }
       ];
     const expectedOutput = [
-      [201, "A Game of Thrones", "Fantasy", "George R.R. Martin", "Bantam", 14.5, 4.5, "https://shorturl.at/jyN45"],
-      [202, "To Kill a Mockingbird", "Fiction", "Harper Lee", "Harper Perennial", 18.86, 4.8, "https://shorturl.at/iovDL"],
-      [203, "The Da Vinci Code", "Mystery", "Dan Brown", "Vintage", 13.05, 4.2, "https://shorturl.at/blDHX"],
-      [204, "Harry Potter and the Order of the Phoenix", "Fantasy", "J.K. Rowling", "Scholastic", 21.76, 4.7, "https://shorturl.at/ivxSX"],
-      [205, "The Great Gatsby", "Classic", "F. Scott Fitzgerald", "Penguin", 11.6, 4, "https://shorturl.at/biQX4"]
+      {
+        "BookId": 201,
+        "BookName": "A Game of Thrones",
+        "Genre": "Fantasy",
+        "Author": "George R.R. Martin",
+        "Publisher": "Bantam",
+        "Price": 14.5,
+        "Ratings": 4.5,
+        "ImageUrl": "https://shorturl.at/jyN45"
+      },
+      {
+        "BookId": 202,
+        "BookName": "To kill a Mockingbird",
+        "Genre": "Fiction",
+        "Author": "Harper Lee",
+        "Publisher": "Harper Perennial",
+        "Price": 18.86,
+        "Ratings": 4.8,
+        "ImageUrl": "https://shorturl.at/iovDL"
+      },
+      {
+        "BookId": 203,
+        "BookName": "The Da Vinci Code",
+        "Genre": "Mystery",
+        "Author": "Dan Brown",
+        "Publisher": "Vintage",
+        "Price": 13.05,
+        "Ratings": 4.2,
+        "ImageUrl": "https://shorturl.at/blDHX"
+      },
+      {
+        "BookId": 204,
+        "BookName": "Harry Potter and the Order of the Phoenix",
+        "Genre": "Fantasy",
+        "Author": "J.K. Rowling",
+        "Publisher": "Scholastic",
+        "Price": 21.76,
+        "Ratings": 4.7,
+        "ImageUrl": "https://shorturl.at/ivxSX"
+      },
+      {
+        "BookId": 205,
+        "BookName": "The Great Gatsby",
+        "Genre": "Classic",
+        "Author": "F. Scott Fitzgerald",
+        "Publisher": "Penguin",
+        "Price": 11.6,
+        "Ratings": 4,
+        "ImageUrl": "https://shorturl.at/biQX4"
+      }
     ];
     const result = irBook(input);
     expect(result).toEqual(expectedOutput);
@@ -62,18 +246,108 @@ describe('inBook test', () => {
   it('should add 18% tax to each item in the result array', () => {
     const input =
       [
-        [201, "A Game of Thrones", "Fantasy", "George R.R. Martin", "Bantam", 11.79, 4.5, "https://shorturl.at/jyN45"],
-        [202, "To Kill a Mockingbird", "Fiction", "Harper Lee", "Harper Perennial", 15.33, 4.8, "https://shorturl.at/iovDL"],
-        [203, "The Da Vinci Code", "Mystery", "Dan Brown", "Vintage", 10.61, 4.2, "https://shorturl.at/blDHX"],
-        [204, "Harry Potter and the Order of the Phoenix", "Fantasy", "J.K. Rowling", "Scholastic", 17.69, 4.7, "https://shorturl.at/ivxSX"],
-        [205, "The Great Gatsby", "Classic", "F. Scott Fitzgerald", "Penguin", 9.43, 4, "https://shorturl.at/biQX4"]
+        {
+          "BookId": 201,
+          "BookName": "A Game of Thrones",
+          "Genre": "Fantasy",
+          "Author": "George R.R. Martin",
+          "Publisher": "Bantam",
+          "Price": 11.79,
+          "Ratings": 4.5,
+          "ImageUrl": "https://shorturl.at/jyN45"
+        },
+        {
+          "BookId": 202,
+          "BookName": "To kill a Mockingbird",
+          "Genre": "Fiction",
+          "Author": "Harper Lee",
+          "Publisher": "Harper Perennial",
+          "Price": 15.33,
+          "Ratings": 4.8,
+          "ImageUrl": "https://shorturl.at/iovDL"
+        },
+        {
+          "BookId": 203,
+          "BookName": "The Da Vinci Code",
+          "Genre": "Mystery",
+          "Author": "Dan Brown",
+          "Publisher": "Vintage",
+          "Price": 10.61,
+          "Ratings": 4.2,
+          "ImageUrl": "https://shorturl.at/blDHX"
+        },
+        {
+          "BookId": 204,
+          "BookName": "Harry Potter and the Order of the Phoenix",
+          "Genre": "Fantasy",
+          "Author": "J.K. Rowling",
+          "Publisher": "Scholastic",
+          "Price": 17.69,
+          "Ratings": 4.7,
+          "ImageUrl": "https://shorturl.at/ivxSX"
+        },
+        {
+          "BookId": 205,
+          "BookName": "The Great Gatsby",
+          "Genre": "Classic",
+          "Author": "F. Scott Fitzgerald",
+          "Publisher": "Penguin",
+          "Price": 9.43,
+          "Ratings": 4,
+          "ImageUrl": "https://shorturl.at/biQX4"
+        }
       ];
     const expectedOutput = [
-      [201, "A Game of Thrones", "Fantasy", "George R.R. Martin", "Bantam", 13.91, 4.5, "https://shorturl.at/jyN45"],
-      [202, "To Kill a Mockingbird", "Fiction", "Harper Lee", "Harper Perennial", 18.09, 4.8, "https://shorturl.at/iovDL"],
-      [203, "The Da Vinci Code", "Mystery", "Dan Brown", "Vintage", 12.52, 4.2, "https://shorturl.at/blDHX"],
-      [204, "Harry Potter and the Order of the Phoenix", "Fantasy", "J.K. Rowling", "Scholastic", 20.87, 4.7, "https://shorturl.at/ivxSX"],
-      [205, "The Great Gatsby", "Classic", "F. Scott Fitzgerald", "Penguin", 11.13, 4, "https://shorturl.at/biQX4"]
+      {
+        "BookId": 201,
+        "BookName": "A Game of Thrones",
+        "Genre": "Fantasy",
+        "Author": "George R.R. Martin",
+        "Publisher": "Bantam",
+        "Price": 13.91,
+        "Ratings": 4.5,
+        "ImageUrl": "https://shorturl.at/jyN45"
+      },
+      {
+        "BookId": 202,
+        "BookName": "To kill a Mockingbird",
+        "Genre": "Fiction",
+        "Author": "Harper Lee",
+        "Publisher": "Harper Perennial",
+        "Price": 18.09,
+        "Ratings": 4.8,
+        "ImageUrl": "https://shorturl.at/iovDL"
+      },
+      {
+        "BookId": 203,
+        "BookName": "The Da Vinci Code",
+        "Genre": "Mystery",
+        "Author": "Dan Brown",
+        "Publisher": "Vintage",
+        "Price": 12.52,
+        "Ratings": 4.2,
+        "ImageUrl": "https://shorturl.at/blDHX"
+      },
+      {
+        "BookId": 204,
+        "BookName": "Harry Potter and the Order of the Phoenix",
+        "Genre": "Fantasy",
+        "Author": "J.K. Rowling",
+        "Publisher": "Scholastic",
+        "Price": 20.87,
+        "Ratings": 4.7,
+        "ImageUrl": "https://shorturl.at/ivxSX"
+      },
+      {
+        "BookId": 205,
+        "BookName": "The Great Gatsby",
+        "Genre": "Classic",
+        "Author": "F. Scott Fitzgerald",
+        "Publisher": "Penguin",
+        "Price": 11.13,
+        "Ratings": 4,
+        "ImageUrl": "https://shorturl.at/biQX4"
+      }
     ];
     const result = inBook(input);
     expect(result).toEqual(expectedOutput);
@@ -83,5 +357,67 @@ describe('inBook test', () => {
     const invalidData = null;
     const result = inBook(invalidData);
     expect(result).toEqual({});
+  });
+});
+
+describe('getBook', () => {
+  it('should return a list of book for "US-NC"', async () => {
+    let mockGetBookDetails = [
+      {
+        "BookId": 201,
+        "BookName": "A Game of Thrones",
+        "Genre": "Fantasy",
+        "Author": "George R.R. Martin",
+        "Publisher": "Bantam",
+        "Price": 10.79,
+        "Ratings": 4.5,
+        "ImageUrl": "https://shorturl.at/jyN45"
+      },
+      {
+        "BookId": 202,
+        "BookName": "To kill a Mockingbird",
+        "Genre": "Fiction",
+        "Author": "Harper Lee",
+        "Publisher": "Harper Perennial",
+        "Price": 14.03,
+        "Ratings": 4.8,
+        "ImageUrl": "https://shorturl.at/iovDL"
+      }
+    ];
+    getBookDetails.mockResolvedValue(mockGetBookDetails);
+    let expectedBookDetails = [
+      {
+        "BookId": 201,
+        "BookName": "A Game of Thrones",
+        "Genre": "Fantasy",
+        "Author": "George R.R. Martin",
+        "Publisher": "Bantam",
+        "Price": 11.65,
+        "Ratings": 4.5,
+        "ImageUrl": "https://shorturl.at/jyN45"
+      },
+      {
+        "BookId": 202,
+        "BookName": "To kill a Mockingbird",
+        "Genre": "Fiction",
+        "Author": "Harper Lee",
+        "Publisher": "Harper Perennial",
+        "Price": 15.15,
+        "Ratings": 4.8,
+        "ImageUrl": "https://shorturl.at/iovDL"
+      }
+    ];
+    const req = { params: { location: 'US-NC' } };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      send: jest.fn(),
+    };
+    await getBook(req, res);
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.send).toHaveBeenCalledWith({
+      success: true,
+      message: 'List of Book',
+      body: expectedBookDetails
+    });
   });
 });
